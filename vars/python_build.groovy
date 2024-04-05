f1e0360e979c47936ff07cece37595212349b127
Hanieh <119368538+haniehsadgh@users.noreply.github.com>
2024-04-04 18:01:49 -0700

    Update python_build.groovy

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -34,7 +34,7 @@ def call(dockerRepoName, imageName, portNum) {
             stage('Security') {
                 steps {
                     script {
-                        sh "/snap/bin/trivy --exit-code 0 --severity HIGH,MEDIUM haniehgh/${dockerRepoName}:${imageName}"
+                        sh "trivy --exit-code 0 --severity HIGH,MEDIUM haniehgh/${dockerRepoName}:${imageName}"
                         // def currentDir = pwd().split('/').last()
                         // sh "bandit -r ${currentDir}/*.py" 
                     }
